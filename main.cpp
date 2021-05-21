5ce2a44e8b16412b6221a8bfaf7f9eb59315a799
A284Philipi <78768392+A284Philipi@users.noreply.github.com>
2021-05-20 21:23:17 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    int casos, i;
+    string pergunta;
+    cin >> casos;
+    fflush(stdin);
+    for (i = 0; i < casos; i++){
+        getline(cin, pergunta);
+        cout << "gzuz" <<endl;
+    }
+}
